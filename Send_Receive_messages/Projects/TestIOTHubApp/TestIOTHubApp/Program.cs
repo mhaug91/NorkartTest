b9f76107d14d4362fbff879e10865369a364b8bf
Erik Wiker <eriwik@norkart.no>
2016-06-13 15:46:20 +0200

    Tutorial for sending and

---
+++ Send_Receive_messages/Projects/TestIOTHubApp/TestIOTHubApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace TestIOTHubApp
+{
+    class Program
+    {
+
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=Teest.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ZvPc1OsXzK+J8HBaIov1CERAq+cWviAog3plbgQ1xBY=";
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceID = "TestDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceID));
+            }
+            catch(DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceID);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
---
+++ Send_Receive_messages/Projects/TestReadFromHub/TestReadFromHub/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using System.Threading;
+
+namespace TestReadFromHub
+{
+    class Program
+    {
+        static string connectionString = "HostName=Teest.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ZvPc1OsXzK+J8HBaIov1CERAq+cWviAog3plbgQ1xBY=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition, CancellationToken ct)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
+            while (true)
+            {
+                if (ct.IsCancellationRequested) break;
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.ASCII.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Receive messages. Ctrl-C to exit.\n");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            System.Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+                Console.WriteLine("Exiting...");
+            };
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition, cts.Token));
+            }
+            Task.WaitAll(tasks.ToArray());
+
+        }
+    }
+}
