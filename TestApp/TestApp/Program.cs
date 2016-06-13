d75b989df36d83e7873d3f8a2189e4444822bd80
Martin Barhaug <mhaug91@gmail.com>
2016-06-13 15:08:31 +0200

    gitattributes?

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -8,10 +8,35 @@ namespace TestApp
 {
     class Program
     {
+
+        static public string EncodeTo64(string toEncode)
+
+        {
+
+            byte[] toEncodeAsBytes
+
+                  = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+
+            string returnValue
+
+                  = System.Convert.ToBase64String(toEncodeAsBytes);
+
+            return returnValue;
+
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            //Console.ReadKey();
+
+            String myData = "Here is a string to encode.";
+            string myDataEncoded = EncodeTo64(myData);
+
+            Console.WriteLine(myDataEncoded);
             Console.ReadKey();
+
+
 
 
         }
