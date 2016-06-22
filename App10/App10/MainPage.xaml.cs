3a831935d15472f386a5a86ec9608735b6b1201d
Erik Wiker <eriwik@norkart.no>
2016-06-22 12:53:01 +0200

    I\m the best, around, nothing is ever gonna keep me down

---
+++ App10/App10/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App10
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        
+
+        static DeviceClient deviceClient;
+        static string iotHubUri = "Teest.azure-devices.net";
+        static string deviceKey = "U2jMw+4VOZiqZWrMwRqRgr98LhNpZUkbeMnx+GsEkEE=";
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "testnorkart",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+            
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("testnorkart", deviceKey));
+
+            SendDeviceToCloudMessagesAsync();
+        }
+    }
+}
